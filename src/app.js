a5bc2e3da8b61a0828ad1c2d6522d90a504ce5d2
Bianca11810 <Bianca11810@gmail.com>
2022-09-14 00:39:31 +0000

    donegit add .

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let div = document.querySelector("#container");
+  let pronoun = ["The", "Our"];
+  let adj = ["Big", "Magnificent"];
+  let noun = ["Boi", "Mango"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let b = 0; b < noun.length; b++) {
+        //create the domain
+        let domain = pronoun[i] + "" + adj[j] + "" + noun[b] + "" + ".com";
+
+        // create a new html element
+        let p = document.createElement("p");
+        // add domain as value to the element
+        p.innerHTML = domain;
+
+        // add element to html
+        div.appendChild(p);
+      }
+    }
+  }
 };
